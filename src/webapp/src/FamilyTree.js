023567008d8c75ea72c31ff7c49dd75eb24f524c
Kristiina Keelmann <kristiina.keelmann@gmail.com>
2020-03-31 22:07:09 +0300

    Updated family tree UI

--- src/webapp/src/FamilyTree.js
+++ src/webapp/src/FamilyTree.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
-import {Container, Spinner} from 'reactstrap';
-import FamilyTreeTemplate from './familytreetemplate.svg';
+import {Card, Spinner} from 'reactstrap';
+import FamilyTreeTemplate from './illustrations/familytree_template.png';
 
 
 class FamilyTree extends Component {
@@ -61,46 +61,33 @@ class FamilyTree extends Component {
         const motherFatherName = familyMembers.motherFatherName;
         const fatherMotherName = familyMembers.fatherMotherName;
         const fatherFatherName = familyMembers.fatherFatherName;
+        const sisters = familyMembers.sisters;
+        const brothers = familyMembers.brothers;
         const children = familyMembers.children;
 
-        let firstSister;
-        let secondSister;
-        const sisters = familyMembers.sisters;
-        if (sisters.length > 0) {
-            firstSister = sisters[0];
-        }
-        if (sisters.length > 1) {
-            secondSister = sisters[1];
-        }
-
-        let firstBrother;
-        let secondBrother;
-        const brothers = familyMembers.brothers;
-        if (brothers.length > 0) {
-            firstBrother = brothers[0];
-        }
-        if (brothers.length > 0) {
-            secondBrother = brothers[1];
-        }
-
         return <div>
-            <Container>
-                <div style={{position: "relative", width: "1500px"}}>
-                    <img src={FamilyTreeTemplate} alt="Family Tree"/>
-                    <div style={{position: "absolute", top: "290px", left: "580px"}}> {selectedPersonName} </div>
-                    <div style={{position: "absolute", top: "200px", left: "600px"}}> {motherName} </div>
-                    <div style={{position: "absolute", top: "200px", left: "750px"}}> {fatherName} </div>
-                    <div style={{position: "absolute", top: "115px", left: "250px"}}> {motherMotherName} </div>
-                    <div style={{position: "absolute", top: "115px", left: "400px"}}> {motherFatherName} </div>
-                    <div style={{position: "absolute", top: "115px", left: "1000px"}}> {fatherMotherName} </div>
-                    <div style={{position: "absolute", top: "115px", left: "1150px"}}> {fatherFatherName} </div>
-                    <div style={{position: "absolute", top: "290px", left: "250px"}}> {firstSister} </div>
-                    <div style={{position: "absolute", top: "290px", left: "400px"}}> {secondSister} </div>
-                    <div style={{position: "absolute", top: "290px", left: "750px"}}> {firstBrother} </div>
-                    <div style={{position: "absolute", top: "290px", left: "930px"}}> {secondBrother} </div>
-                    <div style={{position: "absolute", top: "390px", left: "500px"}}> {children} </div>
-                </div>
-            </Container>
+            <Card>
+                    <div style={{position: "relative", width: "1000px"}}>
+                    <img src={FamilyTreeTemplate} alt="Family Tree" style={{width: "1100px", height: "1000px"}}/>
+                    <div style={{position: "absolute", top: "630px", left: "480px", color: "purple", fontWeight: "bold"}}> {selectedPersonName} </div>
+                    <div style={{position: "absolute", top: "530px", left: "400px"}}> {motherName} </div>
+                    <div style={{position: "absolute", top: "530px", left: "600px"}}> {fatherName} </div>
+                    <div style={{position: "absolute", top: "430px", left: "150px"}}> {motherMotherName} </div>
+                    <div style={{position: "absolute", top: "430px", left: "300px"}}> {motherFatherName} </div>
+                    <div style={{position: "absolute", top: "430px", left: "750px"}}> {fatherMotherName} </div>
+                    <div style={{position: "absolute", top: "430px", left: "900px"}}> {fatherFatherName} </div>
+                    <div style={{position: "absolute", top: "630px", left: "150px"}}> {sisters.length  > 0 && sisters[0]} </div>
+                    <div style={{position: "absolute", top: "630px", left: "350px"}}> {sisters.length  > 1 && sisters[1]}  </div>
+                    <div style={{position: "absolute", top: "630px", left: "50px"}}> {sisters.length  > 2 && sisters[2]}  </div>
+                    <div style={{position: "absolute", top: "630px", left: "700px"}}> {brothers.length  > 0 && brothers[0]} </div>
+                    <div style={{position: "absolute", top: "630px", left: "900px"}}> {brothers.length  > 1 && brothers[1]} </div>
+                    <div style={{position: "absolute", top: "730px", left: "480px"}}> {children.length  > 0 && children[0]} </div>
+                    <div style={{position: "absolute", top: "780px", left: "480px"}}> {children.length  > 1 && children[1]} </div>
+                    <div style={{position: "absolute", top: "830px", left: "480px"}}> {children.length  > 2 && children[2]} </div>
+                    <div style={{position: "absolute", top: "880px", left: "480px"}}> {children.length  > 3 && children[3]} </div>
+                    <div style={{position: "absolute", top: "930px", left: "480px"}}> {children.length  > 4 && children[4]} </div>
+                    </div>
+            </Card>
         </div>
     }
 }
